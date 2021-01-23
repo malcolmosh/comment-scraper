22f3c7bca869e195227eed141040d3d945f95092
Zhijia Chen <zhijia.chen@temple.edu>
2021-01-23 01:13:11 -0500

    .

---
+++ schema.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS url_queue;
+CREATE TABLE url_queue (
+    url VARCHAR(512) PRIMARY KEY,
+    date_hash CHAR(19),
+    host VARCHAR(128),
+    title VARCHAR(256),
+    topic VARCHAR(256),
+    published_time TIMESTAMP,
+    insert_time TIMESTAMP DEFAULT NOW(),
+    status int DEFAULT 0,
+    last_work_time TIMESTAMP, -- NOW() when status 0 -> 1
+    last_finish_time TIMESTAMP, -- NOW() when status 1 -> 2
+    hit_comment BOOLEAN,
+    error_message VARCHAR
+);
